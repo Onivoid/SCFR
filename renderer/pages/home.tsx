c93a469616db5b576adc02ee78f9a469e066c0f5
onivoid <skullyfox.yt@gmail.com>
2024-01-23 00:46:55 +0100

    v1.4.0

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -22,21 +22,16 @@ function Home() {
     <React.Fragment>
       <Head>
         <title>SCFR | Accueil</title>
-        <script type="module" src="https://get.microsoft.com/badge/ms-store-badge.bundled.js"></script>
       </Head>
       <div className='flex flex-col min-h-screen w-full justify-center items-center py-20'>
         <img className='w-1/5'
           src="/images/Logo.png" alt="Osint Companion Logo" />
         <h1 className='text-2xl text-blue-500'>StarCitizen FR</h1>
         <span className='text-sm text-blue-400'>Utilitaire d'installation pour la traduction française de StarCitizen</span>
-        <p className='mt-4 mb-5 text-lg text-red-500 animate-pulse'>Le nouveau logiciel est maintenant disponible sur le Microsoft Store !</p>
-        <ms-store-badge
-          productid="9NN3NXQLWQCF"
-          window-mode="pop up"
-          theme="light"
-          language="fr-fr"
-          animation="on">
-        </ms-store-badge>
+        <p className='mt-4 mb-2 text-lg text-red-500 animate-pulse'>Le nouveau logiciel est maintenant disponible sur le Microsoft Store !</p>
+        <a href="ms-windows-store://pdp/?ProductId=9NN3NXQLWQCF&mode=mini">
+          <img src="https://get.microsoft.com/images/fr-fr%20light.svg" style={{height: "50px"}}/>
+        </a>
       </div>
     </React.Fragment>
   );
